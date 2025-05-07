6eb189713aac83e11bdc8aa98c083f2f8d89c6a8
Mravacardo <matthewheathcote@icloud.com>
2025-05-07 16:58:17 +0100

    Main.py

--- Main.py
+++ Main.py
@@ -48,3 +48,104 @@ def get_text(i, j, gb, l1, l2):
 
 
 def isfree(i, j):
+    return board[i][j] == " "
+
+def isfull():
+    flag = True
+    for i in board:
+        if (i.count(' ') > 0):
+            flag = False
+    return flag
+
+def gameboard_pl(game_board, l1, l2):
+    global button
+    button = []
+    for i in range(3):
+        m = 3 + i
+        button.append(i)
+        button[i] = []
+        for j in range(3):
+            n = j
+            button[i].append(j)
+            get_t = partial(get_text, i, j, game_board, l1, l2)
+            button[i][j] = Button(
+                game_board, bd=5, command=get_t, height=4, width=8)
+            button[i][j].grid(row=m, column=n)
+    game_board.mainloop()
+
+def pc():
+    possiblemove = []
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if board[i][j] == ' ':
+                possiblemove.append([i, j])
+    move = []
+    if possiblemove == []:
+        return
+    else:
+        for let in ['O', 'X']:
+            for i in possiblemove:
+                boardcopy = deepcopy(board)
+                boardcopy[i[0]][i[1]] = let
+                if winner(boardcopy, let):
+                    return i
+        corner = []
+        for i in possiblemove:
+            if i in [[0, 0], [0, 2], [2, 0], [2, 2]]:
+                corner.append(i)
+        if len(corner) > 0:
+            move = random.randint(0, len(corner) - 1)
+            return corner[move]
+        edge = []
+        return edge[move]
+
+def get_text_pc(i, j, gb, l1, l2):
+    global sign
+    if board[i][j] == ' ':
+        if sign % 2 == 0:
+            l1.config(state=DISABLED)
+            l2.config(state=ACTIVE)
+            board[i][j] = "x"
+        else:
+            button[i][j].config(state=ACTIVE)
+            l2.config(state=DISABLED)
+            l1.config(state=ACTIVE)
+            board[i][j] = "O"
+        sign += 1
+        button[i][j].config(text=board[i][j])
+    x = True
+    if winner(board, "X"):
+        gb.destroy()
+        x = False
+        box = messagebox.showinfo("Winner", "Player won the match")
+    elif winner(board, "O"):
+        gb.destroy()
+        x = False
+        box = messagebox.showinfo("Winner", "Computer won the match")
+    elif (isfull()):
+        gb.destroy()
+        x = False
+        box = messagebox.showinfo("Tie Game", "Tie Game")
+    if (x):
+        if sign % 2 != 0:
+            move = pc()
+            button[move[0]][move[1]].config(state=DISALED)
+            get_text_pc(move[0], move[1], gb, l1, l2)
+
+def gameboard_pc(gameboard, l1, l2):
+    global button
+    button = []
+    for i in range(3):
+        m = 3 + i
+        button.append(i)
+        button[i] = []
+        for j in range(3):
+            n = j
+            button[i].append(j)
+            get_t = partial(get_text_pc, i, j, game_board, l1, l2)
+            button[i][j] = Button(
+                game_board, bd=5, command=get_t, height=4, width=8)
+            button[i][j].grid(row=m, column=n)
+    game_board.mainloop()
+
+    
